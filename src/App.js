4db7413e2aff88905c0808eb175b7c6ce035182f
yidaiyinghao <1499326562@qq.com>
2020-12-17 11:20:41 +0800

    集成antd-mobile并且实现样式按需加载

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@
 
 import React from 'react'
 
+import { Button, WhiteSpace, WingBlank } from 'antd-mobile';
+
 export default function App() {
     return (
         <div>
             首页
+
+            <Button type="primary">点我</Button>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
+// import 'antd-mobile/dist/antd-mobile.css';
+
 import App from './App';
+
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
