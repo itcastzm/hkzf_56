7b88f47680e2a872d0a39d76554df062cea0a503
yidaiyinghao <1499326562@qq.com>
2020-12-17 11:48:41 +0800

    配置路由

--- src/App.js
+++ src/App.js
@@ -2,14 +2,29 @@
 
 import React from 'react'
 
-import { Button, WhiteSpace, WingBlank } from 'antd-mobile';
+import { HashRouter, BrowserRouter, Route, Link } from 'react-router-dom';
+
+// 导入页面组件
+import Home from './pages/Home'
+import CityList from './pages/CityList';
 
 export default function App() {
     return (
-        <div>
-            首页
+        <HashRouter>
+            {/* <Button type="primary">点我</Button> */}
+            {/* 导航 */}
+            <ul>
+                <li>
+                    <Link to="/home" >首页</Link>
+                </li>
+                <li>
+                    <Link to="/citylist" >城市选择页面</Link>
+                </li>
+            </ul>
 
-            <Button type="primary">点我</Button>
-        </div>
+            {/* 路径和页面组件的关系 */}
+            <Route path="/home" component={Home} />
+            <Route path="/citylist" component={CityList} />
+        </HashRouter>
     )
 }
