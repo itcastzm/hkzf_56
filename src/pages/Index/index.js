6dbd28c02a51ed9b84d02663ecba5af985172b0a
yidaiyinghao <1499326562@qq.com>
2020-12-18 15:59:02 +0800

    最新资讯

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 
 // 导入轮播图组件
-import { Carousel, Flex, Grid } from 'antd-mobile';
+import { Carousel, Flex, Grid, WingBlank } from 'antd-mobile';
 
 // 引入axios 
 import axios from 'axios';
@@ -33,12 +33,14 @@ export default class Index extends Component {
 
     state = {
         swipers: [],
-        groups: []
+        groups: [],
+        news: []
     }
     componentDidMount() {
         // 请求轮播图数据
         this.getSwipersData();
         this.getGroups();
+        this.getNewsData();
     }
 
     async getSwipersData() {
@@ -59,6 +61,18 @@ export default class Index extends Component {
 
         this.setState({
             groups: res.data.body
+        });
+    }
+
+    async getNewsData() {
+        const res = await axios.get(`http://localhost:8080/home/news`, {
+            params: {
+                area: 'AREA%7C88cff55c-aaa4-e2e0'
+            }
+        });
+
+        this.setState({
+            news: res.data.body
         });
     }
 
@@ -101,6 +115,7 @@ export default class Index extends Component {
         return (
             <Flex className="flex-nav">
                 { navs.map((item, i) => (
+                    // flex: 1
                     <Flex.Item key={i} onClick={() => this.props.history.push(item.path)}>
                         <img src={item.img} alt="nav" />
                         <h3>{item.title}</h3>
@@ -126,13 +141,10 @@ export default class Index extends Component {
 
                 {/* 租房小组     */}
                 <div className="group">
-
                     <h3 className="group-title">
                         租房小组<span className="more">更多</span>
-
                     </h3>
-
-                    <Grid data={this.state.groups} activeStyle={false}
+                    <Grid data={this.state.groups} activeStyle={true}
                         columnNum={2}
                         hasLine={false}
                         square={false}
@@ -151,11 +163,32 @@ export default class Index extends Component {
                             )
                         }
                     />
-
-
-
                 </div>
 
+
+                {/* 最新资讯 */}
+                <div className="news">
+                    <h3 className="group-title">
+                        最新资讯
+                    </h3>
+                    <WingBlank>
+                        {this.state.news.map((item) => (
+                            <div className="news-item">
+                                <div className="img-wrap">
+                                    <img alt="" src={'http://localhost:8080' + item.imgSrc} />
+                                </div>
+                                <Flex className="content" direction="column" justify="between" >
+                                    <div className="title">{item.title}</div>
+                                    <Flex className="desc" justify="between">
+                                        <span>{item.from}</span>
+                                        <span>{item.date}</span>
+                                    </Flex>
+                                </Flex>
+                            </div>
+                        ))}
+                    </WingBlank>
+
+                </div>
             </div>
         )
     }
