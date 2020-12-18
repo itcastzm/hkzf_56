4ef215d80d804e5a5d24f88fec494026b4a0ee51
yidaiyinghao <1499326562@qq.com>
2020-12-18 10:23:14 +0800

    导航区实现

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
 
 // 导入轮播图组件
-import { Carousel, WingBlank } from 'antd-mobile';
+import { Carousel, Flex } from 'antd-mobile';
 
 // 引入axios 
 import axios from 'axios';
+
+// 导入图片
+import Nav1 from '../../assets/images/nav-1.png';
+import Nav2 from '../../assets/images/nav-2.png';
+import Nav3 from '../../assets/images/nav-3.png';
+import Nav4 from '../../assets/images/nav-4.png';
+
+
+import './index.css';
+
 
 
 export default class Index extends Component {
@@ -32,13 +42,12 @@ export default class Index extends Component {
         });
     }
 
+    // 渲染轮播图
+    renderSwipers() {
 
-    render() {
-        return (
-            <div>
-
-
-                {this.state.swipers.length ? <Carousel
+        if (this.state.swipers.length) {
+            return (
+                <Carousel
                     autoplay={true}
                     autoplayInterval={1000}
                     infinite
@@ -61,7 +70,44 @@ export default class Index extends Component {
                             />
                         </a>
                     ))}
-                </Carousel> : null}
+                </Carousel>
+            )
+        }
+
+        return null;
+    }
+
+
+    render() {
+        return (
+            <div className="index-wrapper">
+
+                {/* 轮播图区域 */}
+                <div className="swiper-area">
+                    {this.renderSwipers()}
+                </div>
+
+                {/* 导航区 */}
+                <div className="navs">
+                    <Flex className="flex-nav">
+                        <Flex.Item>
+                            <img src={Nav1} alt="nav" />
+                            <h3>整租</h3>
+                        </Flex.Item>
+                        <Flex.Item>
+                            <img src={Nav2} alt="nav" />
+                            <h3>合租</h3>
+                        </Flex.Item>
+                        <Flex.Item>
+                            <img src={Nav3} alt="nav" />
+                            <h3>地图找房</h3>
+                        </Flex.Item>
+                        <Flex.Item>
+                            <img src={Nav4} alt="nav" />
+                            <h3>去出租</h3>
+                        </Flex.Item>
+                    </Flex>
+                </div>
 
             </div>
         )
