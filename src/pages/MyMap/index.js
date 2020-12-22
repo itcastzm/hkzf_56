3c82281536315c037f154a73b822cc824c3be926
yidaiyinghao <1499326562@qq.com>
2020-12-22 09:07:28 +0800

    展示地图到当前城市

--- src/pages/MyMap/index.js
+++ src/pages/MyMap/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import NavHeader from '../../components/NavHeader';
+import { getCurrentCityInfo } from '../../utils'
 
 // Set  
 // Map 
@@ -11,15 +12,37 @@ import styles from './index.module.scss';
 
 export default class MyMap extends Component {
 
-    componentDidMount() {
+    async componentDidMount() {
+        // 1. 让地图展示当前城市
+        const cityInfo = await getCurrentCityInfo();
+
         var map = new window.BMap.Map("container");
-        var point = new window.BMap.Point(116.404, 39.915);
-        map.centerAndZoom(point, 15);
+        // 创建地址解析器实例     
+        var myGeo = new window.BMap.Geocoder();
+        // 将地址解析结果显示在地图上，并调整地图视野    
+        myGeo.getPoint(cityInfo.label, function (point) {
+
+            if (point) {
+                // 2. 设置地图的缩放级别为11
+                map.centerAndZoom(point, 11);
+                // 3. 添加比例尺和平移缩放控件
+                // 添加平移缩放控件
+                map.addControl(new window.BMap.NavigationControl());
+                // 添加比例尺
+                map.addControl(new window.BMap.ScaleControl());
+
+            }
+        }, cityInfo.label);
+
+
+
+
     }
 
 
     render() {
         return (
+            // BEM 文件名   样式类名   哈希值
             <div className={styles.map}>
                 <NavHeader >地图找房</NavHeader>
                 <div id="container" className={styles.container} ></div>
