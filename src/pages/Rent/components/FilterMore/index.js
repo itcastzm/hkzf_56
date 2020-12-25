73ed3cf93c19e40b33814d00fb864080b8e427a4
yidaiyinghao <1499326562@qq.com>
2020-12-25 11:42:10 +0800

    FilterMore组件点击高亮效果

--- src/pages/Rent/components/FilterMore/index.js
+++ src/pages/Rent/components/FilterMore/index.js
@@ -5,10 +5,40 @@ import FilterFooter from '../../../../components/FilterFooter'
 import styles from './index.module.css'
 
 export default class FilterMore extends Component {
+    state = {
+        selectList: []
+    }
+
+    handleClick(item) {
+
+        const { selectList } = this.state;
+        if (selectList.indexOf(item.value) > -1) {
+            //这个值 item.value 已经被选中了 那么需要取消选中
+            let index = selectList.findIndex((key) => key === item.value);
+            selectList.splice(index, 1);
+        } else {
+            // item.value这个值 没有被选中  那么可以选中
+            selectList.push(item.value);
+        }
+
+        this.setState({
+            selectList
+        });
+    }
+
     // 渲染标签
     renderFilters(list) {
         // 高亮类名： styles.tagActive
-        return list.map(item => <span key={item.value} className={[styles.tag, styles.tagActive].join(' ')}>{item.label}</span>)
+
+        return list.map(item => {
+            let isSelected = this.state.selectList.indexOf(item.value) > -1;
+            return <span key={item.value}
+                onClick={this.handleClick.bind(this, item)}
+                className={[styles.tag, isSelected ? styles.tagActive : ''].join(' ')}
+            >
+                {item.label}
+            </span>
+        })
 
     }
 
